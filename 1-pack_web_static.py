3e85873c55907b7dc318358a81b2a356d6e2a359
Y4h14 <yahyasadig2013@gmail.com>
2024-03-08 01:12:19 +0300

    fixed a typo in task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,9 +9,10 @@ def do_pack():
     """ creates a .tgz archive """
     dt = datetime.utcnow()
     file_name = f"versions/web_static_{dt.year}{dt.month}{dt.day}{dt.hour}"
-    if isdir("versions") is Fasle:
+    if isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
+
     if local(f"tar -czvf {file_name} web_static").failed is True:
         return None
     else:
