4d096ec355ae34d7d732c5b0fea8a44a2b6bdecf
Y4h14 <yahyasadig2013@gmail.com>
2024-03-08 01:43:33 +0300

    updated task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,10 +8,12 @@ from os.path import isdir
 def do_pack():
     """ creates a .tgz archive """
     dt = datetime.utcnow()
-    file_name = "versions/web_static_{}{}{}{}.tgz".format(dt.year,
+    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
                                                           dt.month,
                                                           dt.day,
-                                                          dt.hour)
+                                                          dt.hour,
+                                                          dt.minute,
+                                                          dt.second)
     if isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
