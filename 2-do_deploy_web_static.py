5b4c062c02eddde90dfb945e791d13ff9a5f692d
Y4h14 <yahyasadig2013@gmail.com>
2024-03-08 02:21:11 +0300

    fixed typos in task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,14 +2,14 @@
 """defines a function that distribute an archive"""
 import os.path
 from fabric.api import env, put, run
-env.host = ['34.203.29.40', '54.164.120.187']
+env.hosts = ['34.203.29.40', '54.164.120.187']
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server"""
     if os.path.isfile(archive_path) is False:
         return False
-    file = archive_bath.split('/')[-1]
+    file = archive_path.split('/')[-1]
     name = file.split('.')[0]
 
     if put(arvhive_path, "/tmp/{}".format(file)).failed is True:
@@ -19,7 +19,7 @@ def do_deploy(archive_path):
     if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -c /data/web_static/releases/{}/".
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
