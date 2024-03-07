b811137a751c71a294f701d66da3e28aa32e6907
Y4h14 <yahyasadig2013@gmail.com>
2024-03-08 01:07:03 +0300

    added task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""generates a .tgz archive from a folder"""
+from fabric.api import local
+from datetime import datetime
+from os.path import isdir
+
+
+def do_pack():
+    """ creates a .tgz archive """
+    dt = datetime.utcnow()
+    file_name = f"versions/web_static_{dt.year}{dt.month}{dt.day}{dt.hour}"
+    if isdir("versions") is Fasle:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local(f"tar -czvf {file_name} web_static").failed is True:
+        return None
+    else:
+        return file_name
