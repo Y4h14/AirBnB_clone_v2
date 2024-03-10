c764143453909549908d84d8a79ea876a13fe2a3
Y4h14 <yahyasadig2013@gmail.com>
2024-03-10 16:09:20 +0300

    moved to the new fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
 """defines a function that distribute an archive"""
 import os.path
-from fabric.api import env, put, run
-env.hosts = ['34.203.29.40', '54.164.120.187']
+from fabric import Connection, task
+remote_hosts = ['34.203.29.40', '54.164.120.187']
 
-
-def do_deploy(archive_path):
+@task
+def do_deploy(c, archive_path):
     """Distributes an archive to a web server"""
-    if os.path.isfile(archive_path) is False:
+    if not os.path.isfile(archive_path):
         return False
-    file = archive_path.split('/')[-1]
-    name = file.split('.')[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current".format(name)).failed is True:
-        return False
+    file = os.path.basename(archive_path)
+    name = os.path.splitext(file)[0]
+
+    for host in remote_hosts:
+        with Connection(c):
+            result = c.put(archive_path, "/tmp/{}".format(file), use_sudo=True)
+            if result.failed:
+                return False
+
+            with c.prefix("sudo"):
+                c.run("rm -rf /data/web_static/releases/{}/".format(name))
+                c.run("mkdir -p /data/web_static/releases/{}/".format(name))
+                c.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+                c.run("rm /tmp/{}".format(file))
+                c.run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+                c.run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+
+            c.run("rm -rf /data/web_static/current")
+            c.run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+
     return True
