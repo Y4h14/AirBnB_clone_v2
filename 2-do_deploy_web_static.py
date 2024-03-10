32fc7faf6cb0644ccb6ba491db89092290da6ecf
Y4h14 <yahyasadig2013@gmail.com>
2024-03-10 18:48:59 +0300

    updated task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""defines a function that distribute an archive"""
+"""Defines a function that distributes an archive"""
+
 import os.path
 from fabric import Connection, task
+
 remote_hosts = ['34.203.29.40', '54.164.120.187']
 
 
@@ -11,9 +13,10 @@ def do_deploy(c, archive_path):
     Distributes an archive to a web server
 
     Args:
+        c (Connection): Fabric Connection object.
         archive_path (str): The path of the archive to distribute.
-    returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
+    Returns:
+        True if all operations have been done correctly, otherwise False.
     """
     if not os.path.isfile(archive_path):
         return False
@@ -21,25 +24,29 @@ def do_deploy(c, archive_path):
     file = os.path.basename(archive_path)
     name = os.path.splitext(file)[0]
 
+    # Upload the archive to the servers
+    with Connection(c) as conn:
+        result = conn.put(archive_path, f"/tmp/{file}", use_sudo=True)
+        if result.failed:
+            return False
+
     for host in remote_hosts:
-        with Connection(c):
-            result = c.put(archive_path, "/tmp/{}".format(file), use_sudo=True)
-            if result.failed:
-                return False
+        with Connection(host) as conn:
+            with conn.prefix("sudo"):
+                # Uncompress the archive
+                conn.run("mkdir -p /data/web_static/releases/{}/".format(name))
+                conn.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+                         .format(file, name))
+                conn.run("rm /tmp/{}".format(file))
 
-            with c.prefix("sudo"):
-                c.run("rm -rf /data/web_static/releases/{}/".format(name))
-                c.run("mkdir -p /data/web_static/releases/{}/".format(name))
-                c.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-                      .format(file, name))
-                c.run("rm /tmp/{}".format(file))
-                c.run("mv /data/web_static/releases/{}/web_static/*\
-                       /data/web_static/releases/{}/".format(name, name))
-                c.run("rm -rf /data/web_static/releases/{}/web_static"
-                      .format(name))
+                # Update symbolic links
+                conn.run("mv /data/web_static/releases/{}/web_static/*\
+                         /data/web_static/releases/{}/".format(name, name))
+                conn.run("rm -rf /data/web_static/releases/{}/web_static"
+                         .format(name))
 
-            c.run("rm -rf /data/web_static/current")
-            c.run("ln -s /data/web_static/releases/{}/\
-                   /data/web_static/current".format(name))
+                conn.run("rm -rf /data/web_static/current")
+                conn.run("ln -s /data/web_static/releases/{}/\
+                         /data/web_static/current".format(name))
 
     return True
