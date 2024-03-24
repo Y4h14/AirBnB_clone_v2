a595715038572949da5062daa2bd6b9a564d7a38
y4h14 <yahyasadig2013@gmail.com>
2024-03-24 03:45:36 +0300

    updated task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" defines a flask app that run on localhost"""
 from models import storage
 from flask import Flask, render_template
 from models.state import State
@@ -6,12 +7,21 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def appcontext_treatdown():
+    """
+    this functions runs after each request regardless of weather
+    an exception was raised or not.
+    used for clean up
+    """
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def state_list():
+    """return all states data"""
     return render_template('7-states_list.html',
                            result=storage.all(State))
-
-
 
 
 if __name__ == '__main__':
